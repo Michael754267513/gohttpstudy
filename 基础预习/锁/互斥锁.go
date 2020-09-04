1ced991b58b9ed8a1eb9806861b23a8f8c3b4c25
hzeng <754267513@qq.com>
2020-09-04 17:04:03 +0800

    锁 部分

---
+++ 基础预习/锁/互斥锁.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+/*
+	简单理解我死后 只有一个人可以获得遗产
+
+	老子有一块蛋糕，你们居然都想吃，你们自己竞争下，最后只有一个人可以获得老子蛋糕
+*/
+func main() {
+	var mutex sync.Mutex
+	fmt.Println("start lock main")
+	mutex.Lock()
+	fmt.Println("get locked main")
+	for i := 1; i <= 3; i++ {
+		go func(i int) {
+			fmt.Println("start lock ", i)
+			mutex.Lock()
+			fmt.Println("get locked ", i)
+		}(i)
+	}
+
+	time.Sleep(time.Second)
+	fmt.Println("Unlock the lock main")
+	mutex.Unlock()
+	fmt.Println("get unlocked main")
+	time.Sleep(time.Second)
+}
---
+++ 基础预习/锁/读写锁.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func wirte(i int) (num int) {
+	num = 10
+	num = i + num
+
+	return num
+}
+
+/*
+	简单点理解，我不死你永远得不到财产，
+	读写锁，写优先，其他读的被锁定，当写锁解锁，就可以执行读操作
+	读写锁的访问控制规则如下：
+		①多个写操作之间是互斥的
+		②写操作与读操作之间也是互斥的
+		③多个读操作之间不是互斥的
+	在这样的控制规则下，读写锁可以大大降低性能损耗
+*/
+func main() {
+	var rwm sync.RWMutex
+	for i := 0; i < 3; i++ {
+		go func(i int) {
+			fmt.Println("try to lock read ", i)
+			rwm.RLock()
+			fmt.Println("get locked ", i)
+			i = wirte(i)
+			time.Sleep(time.Second * 2)
+			fmt.Println("try to unlock for reading ", i)
+			rwm.RUnlock()
+			fmt.Println("unlocked for reading ", i)
+		}(i)
+	}
+	time.Sleep(time.Millisecond * 1000)
+	fmt.Println("try to lock for writing")
+	rwm.Lock()
+	fmt.Println("locked for writing")
+}
